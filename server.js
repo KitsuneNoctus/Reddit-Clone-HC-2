5e9474ba0995c2e88fbe1d01cb51193c4db619b0
Henry Calderon <theceintiant@gmail.com>
2021-04-12 20:08:59 -0700

    Working on this is frustrating. The project has started

---
+++ server.js
@@ -0,0 +1,24 @@
+// Require Libraries
+const express = require('express');
+
+// App Setup
+const app = express();
+
+// Middleware
+const exphbs  = require('express-handlebars');
+
+app.use(express.static('public'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('home')
+});
+  
+
+// Start Server
+
+app.listen(3000, () => {
+  console.log('Reddit Clone on port localhost:3000!');
+});
