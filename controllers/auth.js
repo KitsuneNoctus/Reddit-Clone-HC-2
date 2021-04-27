6857448a3eef40f15a34b616c650c715ddcbadcd
Henry Calderon <theceintiant@gmail.com>
2021-04-26 18:31:42 -0700

    Updated login, signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
 module.exports = (app) => {
@@ -14,6 +15,7 @@ module.exports = (app) => {
     user
       .save()
       .then((user) => {
+          console.log(user)
         var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
         res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
         res.redirect('/');
